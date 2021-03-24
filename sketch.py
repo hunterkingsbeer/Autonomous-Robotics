dc266722a044a3e3e2cdf343b601111a4514188e
heija959 <63938610+heija959@users.noreply.github.com>
2021-03-24 14:20:54 +1300

    Create sketch.py

---
+++ sketch.py
@@ -0,0 +1,6 @@
+desiredRow=math.ceil(desiredTile/15)
+currentRow=math.ceil(currentTileNum/15)
+roundedDesired=desiredTile-(desiredTile%abs(deltaTile[orientation]))
+tileRemainder = desiredTile - (desiredTile % abs(deltaTile[orientation]))
+if math.ceil(desiredTile/15) != math.ceil(currentTileNum/15):
+    outsideRange=True
