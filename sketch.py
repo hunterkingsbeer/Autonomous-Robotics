024ade8e33db3206cfade8963d76722a5cb91c89
hedgeya <>
2021-03-25 17:24:58 +1300

    eyoooo

--- sketch.py
+++ sketch.py
@@ -10,3 +10,16 @@ if math.ceil(desiredTile/15) != math.ceil(currentTileNum/15):
 
 # Tower tile if the tower is detected and you are facing the tower.
 towerTile=keyTile[currentTileNum+deltaTile[orientation]]
+
+# Reflect shit
+def reflect():
+    n = sColor.reflected_light_intensity
+    return n
+
+def luminance(groundTuple):
+    return (groundTuple[0] * 0.2126) + (groundTuple[1] * 0.7152) + (groundTuple[2] * 0.0722)
+
+def testreflect():
+    announce("Luminance: " + str(luminance(sColor.raw())))
+    announce("Reflectivity: " + str(reflect()))
+
